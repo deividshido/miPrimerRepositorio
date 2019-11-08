ef8a56ca10d2dca62fbb6b1e58399e49a387542a
DavidAyalaL <50219055+deividshido@users.noreply.github.com>
2019-11-08 17:28:11 -0600

    Subiendo mi proyecto

---
+++ Arenas/Arenas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Arenas
+//
+//  Created by Laboratorio UNAM-Apple 21 on 11/8/19.
+//  Copyright © 2019 Laboratorio UNAM-Apple 21. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
